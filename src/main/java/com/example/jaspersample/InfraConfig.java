8d3579b23a685c400418f7c30e75a8363d119461
mysd33 <mysd.33@gmail.com>
2025-09-13 09:41:41 +0900

    ロギング拡張機能の修正

--- src/main/java/com/example/jaspersample/InfraConfig.java
+++ src/main/java/com/example/jaspersample/InfraConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.example.fw.common.digitalsignature.config.DigitalSignatureConfigPackage;
 import com.example.fw.common.keymanagement.config.KeyManagementConfigPackage;
+import com.example.fw.common.logging.config.LoggingConfigPackage;
 import com.example.fw.common.objectstorage.config.S3ConfigPackage;
 import com.example.fw.common.reports.config.ReportsConfigPackage;
 
@@ -12,10 +13,10 @@ import com.example.fw.common.reports.config.ReportsConfigPackage;
  * インフラストラクチャ層の設定クラス
  */
 @Configuration
-// 帳票出力機能の設定情報を追加
+// 帳票出力、鍵管理、PDF電子署名、オブジェクトストレージアクセス、ロギング拡張設定を追加
 @ComponentScan(basePackageClasses = { //
         ReportsConfigPackage.class, KeyManagementConfigPackage.class, S3ConfigPackage.class,
-        DigitalSignatureConfigPackage.class })
+        DigitalSignatureConfigPackage.class, LoggingConfigPackage.class })
 public class InfraConfig {
 
 }
