4725643c7622c280a1cd6b2526bc799682d603fd
mysd33 <mysd.33@gmail.com>
2025-12-25 13:47:07 +0900

    一時ファイル管理機能の作成に伴う修正

--- src/main/java/com/example/jaspersample/InfraConfig.java
+++ src/main/java/com/example/jaspersample/InfraConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.example.fw.common.digitalsignature.config.DigitalSignatureConfigPackage;
+import com.example.fw.common.file.config.FileConfigPackage;
 import com.example.fw.common.keymanagement.config.KeyManagementConfigPackage;
 import com.example.fw.common.logging.config.LoggingConfigPackage;
 import com.example.fw.common.objectstorage.config.S3ConfigPackage;
@@ -13,10 +14,10 @@ import com.example.fw.common.reports.config.ReportsConfigPackage;
  * インフラストラクチャ層の設定クラス
  */
 @Configuration
-// 帳票出力、鍵管理、PDF電子署名、オブジェクトストレージアクセス、ロギング拡張設定を追加
+// 帳票出力、鍵管理、PDF電子署名、オブジェクトストレージアクセス、一時ファイル管理、ロギング拡張設定を追加
 @ComponentScan(basePackageClasses = { //
-        ReportsConfigPackage.class, KeyManagementConfigPackage.class, S3ConfigPackage.class,
-        DigitalSignatureConfigPackage.class, LoggingConfigPackage.class })
+        ReportsConfigPackage.class, KeyManagementConfigPackage.class, DigitalSignatureConfigPackage.class,
+        S3ConfigPackage.class, FileConfigPackage.class, LoggingConfigPackage.class })
 public class InfraConfig {
 
 }
